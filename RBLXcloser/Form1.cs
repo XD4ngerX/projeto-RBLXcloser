b4051d9c985bfae96c118edcc146a997b146d52e
XD4ngerX <gustavo08dias@gmail.com>
2023-08-27 17:51:19 -0300

    First Commit
    Programa concluido

---
+++ RBLXcloser/Form1.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+
+namespace RBLXcloser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string processName = "RobloxPlayerBeta";
+            Stopwatch stopwatch = new Stopwatch();
+            this.Hide();
+            MessageBox.Show("Programa funcionando", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            while (true)
+            {
+
+                Process[] processes = Process.GetProcessesByName(processName);
+                if (processes.Length > 0)
+                {
+                    foreach (Process process in processes)
+                    {
+                        stopwatch.Restart();
+                        while (stopwatch.Elapsed.TotalMinutes < 5)
+                        {
+                            // Aguarda um segundo antes de verificar novamente
+                            Thread.Sleep(1000);
+                        }
+                        process.CloseMainWindow(); // Tenta fechar a janela do jogo
+                        if (!process.WaitForExit(5000)) // Aguarda até 5 segundos para que o processo termine
+                        {
+                            process.Kill(); // Se não terminar em tempo, encerra o processo
+                            stopwatch.Stop();
+                            stopwatch.Reset();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
