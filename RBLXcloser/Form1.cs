994be4205e8c9dcfb9b8700f76ac022663869829
XD4ngerX <gustavo08dias@gmail.com>
2023-10-19 17:29:38 -0300

    .

--- RBLXcloser/Form1.cs
+++ RBLXcloser/Form1.cs
@@ -12,11 +12,8 @@ namespace RBLXcloser
         private void Form1_Load(object sender, EventArgs e)
         {
             string processName = "RobloxPlayerBeta";
-            Random mensagem = new Random();
-            String[] msg = ["Eu odeio Roblox D:<", "Pare de jogar isso", "Eu não gosto que você fique fazendo isso >;(", "01bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e26933801bc669c432602b8f215dffd0d620cb3b745605b0237a8334ed593747e1826519d871e2cde0c80efe8af2a52f2f50ee2b9fc4bca2cea1f18c2ad9fcb0e269338"]; 
             Stopwatch stopwatch = new Stopwatch();
             this.Hide();
-            int count = 0;
 
             while (true)
             {
@@ -24,31 +21,20 @@ namespace RBLXcloser
                 Process[] processes = Process.GetProcessesByName(processName);
                 if (processes.Length > 0)
                 {
-                    if (count >= 10)
+                    foreach (Process process in processes)
                     {
-                        MessageBox.Show("Eu avisei...","...",MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        Process.Start("shutdown", "/r /t 0");
-                    }
-                    else
-                    {
-                        foreach (Process process in processes)
+                        stopwatch.Restart();
+                        while (stopwatch.Elapsed.TotalMinutes < 2)
                         {
-                            stopwatch.Restart();
-                            while (stopwatch.Elapsed.TotalMinutes < 2)
-                            {
-                                // Aguarda um segundo antes de verificar novamente
-                                Thread.Sleep(1000);
-                            }
-                            process.CloseMainWindow(); // Tenta fechar a janela do jogo
-                            int i = mensagem.Next(0,3);
-                            MessageBox.Show(msg[i],"Atenção",MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                            count++;
-                            if (!process.WaitForExit(5000)) // Aguarda até 5 segundos para que o processo termine
-                            {
-                                process.Kill(); // Se não terminar em tempo, encerra o processo
-                                stopwatch.Stop();
-                                stopwatch.Reset();
-                            }
+                            // Aguarda um segundo antes de verificar novamente
+                            Thread.Sleep(1000);
+                        }
+                        process.CloseMainWindow(); // Tenta fechar a janela do jogo
+                        if (!process.WaitForExit(5000)) // Aguarda até 5 segundos para que o processo termine
+                        {
+                            process.Kill(); // Se não terminar em tempo, encerra o processo
+                            stopwatch.Stop();
+                            stopwatch.Reset();
                         }
                     }
                 }
